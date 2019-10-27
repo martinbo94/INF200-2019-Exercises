fb95e19df6ffb36f3e4f9c9586376bbeec64ae47
Martin Bø <martinb@nmbu.no>
2019-10-27 21:56:56 +0100

    Finished the exercises

--- src/martin_boe_ex/ex04/myrand.py
+++ src/martin_boe_ex/ex04/myrand.py
@@ -4,25 +4,33 @@ __author__ = 'Martin Bø'
 __email__ = 'martinb@nmbu.no'
 
 
-class LCGrand:
+class LCGRand:
     def __init__(self, seed):
         self.seed = seed
         self.a = 7**5
         self.m = (2**31)-1
 
     def rand(self):
-        pass
+        self.seed = (self.a * self.seed) % self.m
+        return self.seed
 
 
 class ListRand:
     def __init__(self, num_list):
         self.num_list = num_list
+        self.index = -1
 
     def rand(self):
-        if len(self.num_list) is not 0:
-
-            number = self.num_list[0]
-            del(self.num_list[0])
+        self.index += 1
+        if self.index < len(self.num_list):
+            number = self.num_list[self.index]
             return number
         else:
-            raise RuntimeError
+            raise RuntimeError("List is full")
+
+
+if __name__ == "__main__":
+    lcg = LCGRand(52)
+    print(lcg.rand())
+    list_rand = ListRand([3, 2, 1, 5])
+    print(list_rand.rand())
